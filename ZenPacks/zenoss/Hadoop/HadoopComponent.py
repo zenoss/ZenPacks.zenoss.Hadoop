181c12367545fa7f7678a17f7937dac8c0d9851d
Volodymyr Sergeyev <vova.sergeyev@gmail.com>
2014-03-25 14:54:53 +0200

    Init, modeler plugin added

--- ZenPacks/zenoss/Hadoop/HadoopComponent.py
+++ ZenPacks/zenoss/Hadoop/HadoopComponent.py
@@ -1,25 +1,21 @@
-######################################################################
+##############################################################################
 #
 # Copyright (C) Zenoss, Inc. 2014, all rights reserved.
 #
 # This content is made available according to terms specified in
-# License.zenoss under the directory where your Zenoss product is
-# installed.
+# License.zenoss under the directory where your Zenoss product is installed.
 #
-######################################################################
+##############################################################################
 
+from Products.ZenModel.Device import Device
 from Products.ZenModel.DeviceComponent import DeviceComponent
 from Products.ZenModel.ManagedEntity import ManagedEntity
 from Products.ZenModel.ZenossSecurity import ZEN_CHANGE_DEVICE
-from Products.ZenRelations.RelSchema import ToManyCont, ToOne
 
 
-class ExampleComponent(DeviceComponent, ManagedEntity):
-    meta_type = portal_type = "ExampleComponent"
+class HadoopComponent(DeviceComponent, ManagedEntity):
+    ''' Base class for all components in this ZenPack.  '''
 
-    attributeOne = None
-    attributeTwo = None
-	
     # Explicit inheritence.
     _properties = ManagedEntity._properties
     _relations = ManagedEntity._relations
@@ -31,10 +27,12 @@ class ExampleComponent(DeviceComponent, ManagedEntity):
             'name': 'Template',
             'action': 'objTemplates',
             'permissions': (ZEN_CHANGE_DEVICE,),
-        },),
-    },)
+            },),
+        },)
 
-    # Custom components must always implement the device method. The method
-    # should return the device object that contains the component.
-    def device(self):
-        return self.exampleDevice()
+    def getIconPath(self):
+        '''
+        Return the path to an icon for this component.
+        '''
+        return '/++resource++ZenPacks_zenoss_Hadoop/img/%s.png' \
+               % self.meta_type
--- ZenPacks/zenoss/Hadoop/__init__.py
+++ ZenPacks/zenoss/Hadoop/__init__.py
@@ -1,47 +1,133 @@
-# Nothing is required in this __init__.py, but it is an excellent place to do
-# many things in a ZenPack.
+##############################################################################
 #
-# The example below which is commented out by default creates a custom subclass
-# of the ZenPack class. This allows you to define custom installation and
-# removal routines for your ZenPack. If you don't need this kind of flexibility
-# you should leave the section commented out and let the standard ZenPack
-# class be used.
+# Copyright (C) Zenoss, Inc. 2014, all rights reserved.
 #
-# Code included in the global scope of this file will be executed at startup
-# in any Zope client. This includes Zope itself (the web interface) and zenhub.
-# This makes this the perfect place to alter lower-level stock behavior
-# through monkey-patching.
+# This content is made available according to terms specified in
+# License.zenoss under the directory where your Zenoss product is installed.
+#
+##############################################################################
+"""
+Custom ZenPack initialization code. All code defined in this module will be
+executed at startup time in all Zope clients.
+"""
+import math
+import logging
+log = logging.getLogger('zen.Hadoop')
 
-# import Globals
-#
-# from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
-# from Products.ZenUtils.Utils import unused
-#
-# unused(Globals)
-#
-#
-# class ZenPack(ZenPackBase):
-#
-#     # All zProperties defined here will automatically be created when the
-#     # ZenPack is installed.
-#     packZProperties = [
-#         ('zExampleString', 'default value', 'string'),
-#         ('zExampleInt', 411, 'int'),
-#         ('zExamplePassword', 'notsecure', 'password'),
-#         ]
-#
-#     def install(self, dmd):
-#         ZenPackBase.install(self, dmd)
-#
-#         # Put your customer installation logic here.
-#         pass
-#
-#     def remove(self, dmd, leaveObjects=False):
-#         if not leaveObjects:
-#             # When a ZenPack is removed the remove method will be called with
-#             # leaveObjects set to False. This means that you likely want to
-#             # make sure that leaveObjects is set to false before executing
-#             # your custom removal code.
-#             pass
-#
-#         ZenPackBase.remove(self, dmd, leaveObjects=leaveObjects)
+import Globals
+
+from Products.ZenEvents.EventManagerBase import EventManagerBase
+from Products.ZenModel.Device import Device
+from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
+from Products.ZenRelations.RelSchema import ToManyCont, ToOne
+from Products.ZenRelations.zPropertyCategory import setzPropertyCategory
+from Products.ZenUtils.Utils import unused
+from Products.Zuul.interfaces import ICatalogTool
+
+unused(Globals)
+
+
+# Categorize zProperties.
+setzPropertyCategory('zHadoop', 'Hadoop')
+
+# Modules containing model classes. Used by zenchkschema to validate
+# bidirectional integrity of defined relationships.
+productNames = (
+    'HadoopServiceNode',
+    'HadoopDataNode'
+    )
+
+# Useful to avoid making literal string references to module and class names
+# throughout the rest of the ZenPack.
+ZP_NAME = 'ZenPacks.zenoss.Hadoop'
+MODULE_NAME = {}
+CLASS_NAME = {}
+for product_name in productNames:
+    MODULE_NAME[product_name] = '.'.join([ZP_NAME, product_name])
+    CLASS_NAME[product_name] = '.'.join([ZP_NAME, product_name, product_name])
+
+# Define new device relations.
+NEW_DEVICE_RELATIONS = (
+    ('hadoop_service_nodes', 'HadoopServiceNode'),
+    ('hadoop_data_nodes', 'HadoopDataNode'),
+    )
+
+NEW_COMPONENT_TYPES = (
+    'ZenPacks.zenoss.Hadoop.HadoopServiceNode.HadoopServiceNode',
+    'ZenPacks.zenoss.Hadoop.HadoopDataNode.HadoopDataNode',
+    )
+
+# Add new relationships to Device if they don't already exist.
+for relname, modname in NEW_DEVICE_RELATIONS:
+    if relname not in (x[0] for x in Device._relations):
+        Device._relations += (
+            (relname,
+             ToManyCont(ToOne, '.'.join((ZP_NAME, modname)), 'hadoop_host')),
+        )
+
+
+# Add ErrorNotification to Device
+def setErrorNotification(self, msg):
+    if msg == 'clear':
+        self.dmd.ZenEventManager.sendEvent(dict(
+            device=self.id,
+            summary=msg,
+            eventClass='/Status',
+            eventKey='ConnectionError',
+            severity=0,
+            ))
+    else:
+        self.dmd.ZenEventManager.sendEvent(dict(
+            device=self.id,
+            summary=msg,
+            eventClass='/Status',
+            eventKey='ConnectionError',
+            severity=5,
+            ))
+
+    return
+
+
+def getErrorNotification(self):
+    return
+
+Device.setErrorNotification = setErrorNotification
+Device.getErrorNotification = getErrorNotification
+
+
+class ZenPack(ZenPackBase):
+    """
+    ZenPack loader that handles custom installation and removal tasks.
+    """
+
+    packZProperties = [
+        ('zHadoop', False, 'boolean'),
+    ]
+
+    def install(self, app):
+        super(ZenPack, self).install(app)
+
+        log.info('Adding Hadoop relationships to existing devices')
+        self._buildDeviceRelations()
+
+    def remove(self, app, leaveObjects=False):
+        if not leaveObjects:
+            log.info('Removing MySqlMonitor components')
+            cat = ICatalogTool(app.zport.dmd)
+            for brain in cat.search(types=NEW_COMPONENT_TYPES):
+                component = brain.getObject()
+                component.getPrimaryParent()._delObject(component.id)
+
+            # Remove our Device relations additions.
+            Device._relations = tuple(
+                [x for x in Device._relations
+                    if x[0] not in NEW_DEVICE_RELATIONS])
+
+            log.info('Removing Hadoop device relationships')
+            self._buildDeviceRelations()
+
+        super(ZenPack, self).remove(app, leaveObjects=leaveObjects)
+
+    def _buildDeviceRelations(self):
+        for d in self.dmd.Devices.getSubDevicesGen():
+            d.buildRelations()
