7f75e4fa646ee7dc41532bdbaed0b5f89b8797a2
ssirushchyshyn <irushch@softserveinc.com>
2014-09-01 14:52:32 +0300

    Removed double logging in modeler plugins

--- ZenPacks/zenoss/Hadoop/modeler/plugins/HadoopDataNode.py
+++ ZenPacks/zenoss/Hadoop/modeler/plugins/HadoopDataNode.py
@@ -121,7 +121,6 @@ class HadoopDataNode(PythonPlugin):
         except:
             e = failure  # no twisted failure
         e = check_error(e, device.id) or e
-        log.error(e)
         self._send_event(str(e), device.id, 5)
         raise e
 
