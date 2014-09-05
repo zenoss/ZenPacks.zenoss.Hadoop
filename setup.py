7f8fcdb71f0e4f34e5d9d5b9b2a1354e29c94e87
John Causey <jcausey@zenoss.com>
2014-09-05 16:34:05 -0500

    release: version 1.0.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.Hadoop"
-VERSION = "1.0.0dev"
+VERSION = "1.0.0"
 AUTHOR = "Zenoss"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
